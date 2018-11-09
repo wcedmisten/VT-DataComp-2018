429276bc1d20f230a4d6b76a2c6cc9de11610231
Daniel Ocheltree <djo96@vt.edu>
2018-11-09 00:07:04 -0500

    Added 2 plots, started regression

--- DataCompetition.R
+++ DataCompetition.R
@@ -3,8 +3,8 @@ library(hexbin)
 library(corrplot)
 library(dplyr)
 
-thads <- read.csv(file="/home/william/Downloads/thads2011.txt")
-thads_cleaned <- read.csv(file="/home/william/Downloads/thads2011.txt", na.strings=c("-6","'-6'", ".", "-9"))
+thads <- read.csv(file="C:/Users/Daniel/OneDrive/Documents/Virginia Tech/Senior Year/Fall 2018/CMDA 3654/Competition/thads2011.txt")
+thads_cleaned <- read.csv(file="C:/Users/Daniel/OneDrive/Documents/Virginia Tech/Senior Year/Fall 2018/CMDA 3654/Competition/thads2011.txt", na.strings=c("-6","'-6'", ".", "-9"))
 
 # scatter plot with colors
 ggplot(data=thads_cleaned, aes(x=ZSMHC, y=COSTMedRELAMIPCT, color=STRUCTURETYPE)) + geom_point(size=1, alpha=.01)
@@ -12,6 +12,35 @@ ggplot(data=thads_cleaned, aes(x=ZSMHC, y=COSTMedRELAMIPCT, color=STRUCTURETYPE)
 # density plot
 ggplot(data=thads_cleaned, aes(x=ZSMHC, y=COSTMedRELAMIPCT)) + stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
   labs(x = "Monthly Housing Cost", y="Median Mortgage Rate Housing Costs Relative to Median Income")
+
+# FMR Density plot?
+ggplot(data = thads_cleaned, aes(x = FMR, y = COSTMedRELAMIPCT)) + stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
+    labs(x = "Fair Markey Rent", y="Median Mortgage Rate Housing Costs Relative to Median Income")
+
+# Scatter plot Household Income (ZINC2)
+# Change x axis to log?
+ggplot(data = thads_cleaned) +
+    geom_point(aes(x = ZINC2, y = COSTMedRELAMIPCT, col = ZINC2)) +
+    labs(title = "Household Income vs. Affordability\n", x = "Household Income", y = "Median Mortgage Rate Housing Costs Relative to Median Income")
+
+# Barplot for PER
+PER <- sort(unique(thads_cleaned$PER))
+PER_MEAN <- vector()
+PER_MEAN <- c(PER_MEAN, 1:16)
+i <- 1
+for (p in PER) {
+    PER_MEAN[i] <- mean((thads_cleaned$COSTMedRELAMIPCT[thads_cleaned["PER"] == p]), na.rm=TRUE)
+    i <- i + 1
+}
+ggplot() +
+    geom_bar(mapping = aes(x = PER, y = PER_MEAN, fill = as.factor(PER)), stat = "identity") +
+    labs(title = "Affordability by People Per Home\n", x = "People Per Home", y = "Average Median Mortgage Rate Housing Costs Relative to Median Income") +
+    scale_fill_discrete(name = "People in Household")
+
+# regressions lol
+fit1 <- lm( COSTMedRELAMIPCT ~ ZSMHC, data = thads_cleaned )
+fit2 <- lm( COSTMedRELAMIPCT ~ poly(ZSMHC, 2, raw = TRUE), data = thads_cleaned )
+fit3 <- lm( COSTMedRELAMIPCT ~ poly(ZSMHC, 3, raw = TRUE), data = thads_cleaned )
 
 #head(thads)
 #View(head(thads, 50))
